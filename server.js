ca3569e0cae329dc03c94e28f075772c30c943f7
Olena Derevianko <olenka.derevianko@gmail.com>
2023-03-07 16:56:13 +0100

    add file config

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://helen:55judo33@cluster0.afdkbgo.mongodb.net/db_contacts?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
+
 mongoose.set("strictQuery", true);
 
 mongoose
