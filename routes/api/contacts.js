c08b7518c88e323f5cff3067ee8ea11f86fa3029
Olena Derevianko <olenka.derevianko@gmail.com>
2023-03-15 22:23:59 +0100

    add hw4

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
+
+const { schemas } = require("../../models/contact");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
+
 const router = express.Router();
 
-router.get("/", ctrl.allContacts);
+router.get("/", authenticate, ctrl.allContacts);
 
-router.get("/:contactId", ctrl.contactById);
+router.get("/:id", authenticate, isValidId, ctrl.contactById);
 
-router.post("/", ctrl.add);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:contactId", ctrl.contactUpdateById);
+router.put(
+  "/:id",
+  authenticate,
+  isValidId,
+  validateBody(schemas.putSchema),
+  ctrl.contactUpdateById
+);
 
-router.delete("/:contactId", ctrl.deleteContact);
+router.patch(
+  "/:contactId/favorite",
+  authenticate,
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.contactUpdateFavorite
+);
+
+router.delete("/:id", authenticate, isValidId, ctrl.deleteContact);
 
 module.exports = router;
